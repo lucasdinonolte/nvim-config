b835082bc15451e8650f1708c03fb485424efffd
Lucas Nolte <lucas@designsystems.international>
2026-01-06 09:47:20 +0100

    feat: drinking the AI koolaid without leaving neovim

--- init.lua
+++ init.lua
@@ -1,60 +1,60 @@
 -- Basic settings
-vim.opt.number = true                              -- Line numbers
-vim.opt.relativenumber = true                      -- Relative line numbers
-vim.opt.cursorline = true                          -- Highlight current line
-vim.opt.wrap = false                               -- Don't wrap lines
-vim.opt.scrolloff = 10                             -- Keep 10 lines above/below cursor 
-vim.opt.sidescrolloff = 8                          -- Keep 8 columns left/right of cursor
+vim.opt.number = true         -- Line numbers
+vim.opt.relativenumber = true -- Relative line numbers
+vim.opt.cursorline = true     -- Highlight current line
+vim.opt.wrap = false          -- Don't wrap lines
+vim.opt.scrolloff = 10        -- Keep 10 lines above/below cursor
+vim.opt.sidescrolloff = 8     -- Keep 8 columns left/right of cursor
 
 -- Indentation
-vim.opt.tabstop = 2                                -- Tab width
-vim.opt.shiftwidth = 2                             -- Indent width
-vim.opt.softtabstop = 2                            -- Soft tab stop
-vim.opt.expandtab = true                           -- Use spaces instead of tabs
-vim.opt.smartindent = true                         -- Smart auto-indenting
-vim.opt.autoindent = true                          -- Copy indent from current line
+vim.opt.tabstop = 2        -- Tab width
+vim.opt.shiftwidth = 2     -- Indent width
+vim.opt.softtabstop = 2    -- Soft tab stop
+vim.opt.expandtab = true   -- Use spaces instead of tabs
+vim.opt.smartindent = true -- Smart auto-indenting
+vim.opt.autoindent = true  -- Copy indent from current line
 
 -- Search settings
-vim.opt.ignorecase = true                          -- Case insensitive search
-vim.opt.smartcase = true                           -- Case sensitive if uppercase in search
-vim.opt.hlsearch = true                            -- Highlight search results 
-vim.opt.incsearch = true                           -- Show matches as you type
+vim.opt.ignorecase = true -- Case insensitive search
+vim.opt.smartcase = true  -- Case sensitive if uppercase in search
+vim.opt.hlsearch = true   -- Highlight search results
+vim.opt.incsearch = true  -- Show matches as you type
 
 -- Visual settings
-vim.opt.termguicolors = true                       -- Enable 24-bit colors
-vim.opt.signcolumn = "yes"                         -- Always show sign column
-vim.opt.colorcolumn = "100"                        -- Show column at 100 characters
-vim.opt.showmatch = true                           -- Highlight matching brackets
-vim.opt.matchtime = 2                              -- How long to show matching bracket
-vim.opt.cmdheight = 1                              -- Command line height
-vim.opt.showmode = false                           -- Don't show mode in command line 
-vim.opt.pumheight = 10                             -- Popup menu height 
-vim.opt.conceallevel = 0                           -- Don't hide markup 
-vim.opt.concealcursor = ""                         -- Don't hide cursor line markup 
-vim.opt.lazyredraw = true                          -- Don't redraw during macros
-vim.opt.synmaxcol = 300                            -- Syntax highlighting limit
-vim.opt.winborder = 'rounded'                      -- Rounded corners for all floating windows
+vim.opt.termguicolors = true  -- Enable 24-bit colors
+vim.opt.signcolumn = "yes"    -- Always show sign column
+vim.opt.colorcolumn = "100"   -- Show column at 100 characters
+vim.opt.showmatch = true      -- Highlight matching brackets
+vim.opt.matchtime = 2         -- How long to show matching bracket
+vim.opt.cmdheight = 1         -- Command line height
+vim.opt.showmode = false      -- Don't show mode in command line
+vim.opt.pumheight = 10        -- Popup menu height
+vim.opt.conceallevel = 0      -- Don't hide markup
+vim.opt.concealcursor = ""    -- Don't hide cursor line markup
+vim.opt.lazyredraw = true     -- Don't redraw during macros
+vim.opt.synmaxcol = 300       -- Syntax highlighting limit
+vim.opt.winborder = 'rounded' -- Rounded corners for all floating windows
 
 -- Behavior settings
-vim.opt.hidden = true                              -- Allow hidden buffers
-vim.opt.errorbells = false                         -- No error bells
-vim.opt.backspace = "indent,eol,start"             -- Better backspace behavior
-vim.opt.autochdir = false                          -- Don't auto change directory
-vim.opt.iskeyword:append("-")                      -- Treat dash as part of word
-vim.opt.path:append("**")                          -- include subdirectories in search
-vim.opt.selection = "exclusive"                    -- Selection behavior
-vim.opt.mouse = "a"                                -- Enable mouse support
-vim.opt.clipboard:append("unnamedplus")            -- Use system clipboard
-vim.opt.modifiable = true                          -- Allow buffer modifications
-vim.opt.encoding = "UTF-8"                         -- Set encoding
+vim.opt.hidden = true                   -- Allow hidden buffers
+vim.opt.errorbells = false              -- No error bells
+vim.opt.backspace = "indent,eol,start"  -- Better backspace behavior
+vim.opt.autochdir = false               -- Don't auto change directory
+vim.opt.iskeyword:append("-")           -- Treat dash as part of word
+vim.opt.path:append("**")               -- include subdirectories in search
+vim.opt.selection = "exclusive"         -- Selection behavior
+vim.opt.mouse = "a"                     -- Enable mouse support
+vim.opt.clipboard:append("unnamedplus") -- Use system clipboard
+vim.opt.modifiable = true               -- Allow buffer modifications
+vim.opt.encoding = "UTF-8"              -- Set encoding
 
 -- Split behavior
-vim.opt.splitbelow = true                          -- Horizontal splits go below
-vim.opt.splitright = true                          -- Vertical splits go right
+vim.opt.splitbelow = true -- Horizontal splits go below
+vim.opt.splitright = true -- Vertical splits go right
 
 -- Filetree
-vim.g.netrw_liststyle = 3                          -- Tree View
-vim.g.netrw_banner = 0                             -- Remove netrw banner
+vim.g.netrw_liststyle = 3 -- Tree View
+vim.g.netrw_banner = 0    -- Remove netrw banner
 
 
 -- ============================================================================
@@ -63,8 +63,8 @@ vim.g.netrw_banner = 0                             -- Remove netrw banner
 local map = vim.keymap.set
 
 -- Key mappings
-vim.g.mapleader = " "                              -- Set leader key to space
-vim.g.maplocalleader = "\\"                        -- Set local leader key (NEW)
+vim.g.mapleader = " "       -- Set leader key to space
+vim.g.maplocalleader = "\\" -- Set local leader key (NEW)
 
 -- Clear highlights
 map("n", "<Esc>", "<cmd>noh<CR>", { desc = "general clear highlights" })
@@ -105,8 +105,11 @@ map("n", "<leader>tl", function()
   vim.cmd.wincmd("L")
 end)
 
--- Run prettier
+-- Run lsp formatter
 map("n", "<leader>fm", vim.lsp.buf.format)
+
+-- Run prettier
+map("n", "<leader>gp", ":silent %!npx prettier --stdin-filepath %<CR>")
 
 -- ============================================================================
 -- STATUSLINE
@@ -123,7 +126,7 @@ end
 
 -- Git status
 local function git_status()
-  local file = vim.fn.expand("%:p")  -- Get the full path of the current file
+  local file = vim.fn.expand("%:p") -- Get the full path of the current file
   local status = vim.fn.system("git status --porcelain " .. file .. " 2>/dev/null")
 
   if status ~= "" then
@@ -135,17 +138,17 @@ end
 
 -- LSP status
 local function lsp_status()
-    local attached_clients = vim.lsp.get_clients({ bufnr = 0 })
-    if #attached_clients == 0 then
-        return ""
-    end
-    local it = vim.iter(attached_clients)
-    it:map(function (client)
-        local name = client.name:gsub("language.server", "ls")
-        return name
-    end)
-    local names = it:totable()
-    return "[" .. table.concat(names, ", ") .. "]"
+  local attached_clients = vim.lsp.get_clients({ bufnr = 0 })
+  if #attached_clients == 0 then
+    return ""
+  end
+  local it = vim.iter(attached_clients)
+  it:map(function(client)
+    local name = client.name:gsub("language.server", "ls")
+    return name
+  end)
+  local names = it:totable()
+  return "[" .. table.concat(names, ", ") .. "]"
 end
 -- Mode indicator
 local function mode_icon()
@@ -164,26 +167,26 @@ vim.cmd([[
 
 -- Function to change statusline based on window focus
 local function setup_dynamic_statusline()
-  vim.api.nvim_create_autocmd({"WinEnter", "BufEnter"}, {
+  vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
     callback = function()
-    vim.opt_local.statusline = table.concat {
-      "  ",
-      "%#StatusLineBold#",
-      " %{v:lua.mode_icon()} ",
-      "%#StatusLine#",
-      "%{v:lua.git_branch()}",
-      "%{v:lua.git_status()}",
-      " │ %f %h%m%r",
-      " │ %{strlen(&ft)?&ft[0].&ft[1:]:'None'}", -- file type
-      " │ %{v:lua.lsp_status()}",
-      "%=",                     -- Right-align everything after this
-      " │ %l:%c  %P ",             -- Line:Column and Percentage
-    }
+      vim.opt_local.statusline = table.concat {
+        "  ",
+        "%#StatusLineBold#",
+        " %{v:lua.mode_icon()} ",
+        "%#StatusLine#",
+        "%{v:lua.git_branch()}",
+        "%{v:lua.git_status()}",
+        " │ %f %h%m%r",
+        " │ %{strlen(&ft)?&ft[0].&ft[1:]:'None'}", -- file type
+        " │ %{v:lua.lsp_status()}",
+        "%=", -- Right-align everything after this
+        " │ %l:%c  %P ", -- Line:Column and Percentage
+      }
     end
   })
   vim.api.nvim_set_hl(0, "StatusLineBold", { bold = true })
 
-  vim.api.nvim_create_autocmd({"WinLeave", "BufLeave"}, {
+  vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
     callback = function()
       vim.opt_local.statusline = "  %f %h%m%r │ %{strlen(&ft)?&ft[0].&ft[1:]:'None'} | %=  %l:%c   %P "
     end
@@ -244,23 +247,23 @@ vim.diagnostic.config({
 -- ===========================================================================
 -- Prevent editing of things inside of node_modules
 vim.api.nvim_create_autocmd("BufNew", {
-    pattern = {
-        "node_modules/**",
-    },
-    callback = function(event)
-        vim.bo[event.buf].modifiable = false
-    end,
+  pattern = {
+    "node_modules/**",
+  },
+  callback = function(event)
+    vim.bo[event.buf].modifiable = false
+  end,
 })
 
 -- Auto create dir when saving a file, in case some intermediate directory does not exists
 vim.api.nvim_create_autocmd("BufWritePre", {
-    callback = function(event)
-        if event.match:match("^%w%w+://") then
-            return
-        end
-        local file = vim.uv.fs_realpath(event.match) or event.match
-        vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
-    end,
+  callback = function(event)
+    if event.match:match("^%w%w+://") then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
 })
 
 -- ===========================================================================
@@ -268,28 +271,30 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 -- ===========================================================================
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-{
-  "navarasu/onedark.nvim",
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require('onedark').setup {
-      style = 'deep'
-    }
-    require('onedark').load()
-  end
-},
+  -- One Dark Theme
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('onedark').setup {
+        style = 'deep'
+      }
+      require('onedark').load()
+    end
+  },
+  -- OIL (File System Editor)
   {
     'stevearc/oil.nvim',
     opts = {
@@ -300,17 +305,47 @@ require("lazy").setup({
     },
     lazy = false,
   },
+  -- FZF
   {
-        "https://github.com/junegunn/fzf.vim",
-        dependencies = {
-            "https://github.com/junegunn/fzf",
-        },
-        keys = {
-            { "<Leader>ff", "<Cmd>Files<CR>", desc = "Find files" },
-            { "<Leader>fb", "<Cmd>Buffers<CR>", desc = "Find buffers" },
-            { "<Leader>fw", "<Cmd>Rg<CR>", desc = "Search project" },
-        },
+    "https://github.com/junegunn/fzf.vim",
+    dependencies = {
+      "https://github.com/junegunn/fzf",
     },
+    keys = {
+      { "<Leader>ff", "<Cmd>Files<CR>",                                         desc = "Find files" },
+      { "<Leader>fb", "<Cmd>Buffers<CR>",                                       desc = "Find buffers" },
+      { "<Leader>fw", "<Cmd>Rg<CR>",                                            desc = "Search project" },
+
+      -- Specific fzf commands, to make working with the notes stored in my
+      -- obsidian a bit simpler, so I don't need to context switch in order
+      -- to access my notes
+      --
+      -- TODO: This should be moved to an env variable, so it's independent
+      -- of the machine that I'm running neovim on.
+      { "<Leader>no", "<Cmd>Files " .. (os.getenv("OBS_PATH") or "") .. "<CR>", desc = "Search files in notes" },
+    },
+  },
+  -- Copilot
+  -- Completions
+  {
+    "github/copilot.vim",
+  },
+  -- Copilot Chat
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    keys = {
+      { "<leader>cc", "<Cmd>CopilotChat<CR>",         desc = "Open Copilot Chat",                     mode = { "n", "v" } },
+      { "<leader>ct", "<Cmd>CopilotChatTests<CR>",    desc = "Write Tests for Selected Code",         mode = { "n", "v" } },
+      { "<leader>cf", "<Cmd>CopilotChatFix<CR>",      desc = "Identify bugs in selected code",        mode = { "n", "v" } },
+      { "<leader>cr", "<Cmd>CopilotChatReview<CR>",   desc = "Review Selected Code",                  mode = { "n", "v" } },
+      { "<leader>cd", "<Cmd>CopilotChatDocs<CR>",     desc = "Write Docs Comments for selected code", mode = { "n", "v" } },
+      { "<leader>ci", "<Cmd>CopilotChatOptimize<CR>", desc = "Improve performance and readability",   mode = { "n", "v" } },
+    },
+  },
 })
 
 require("oil").setup()
